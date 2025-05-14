77da33b8d7cf0e4b2228cb6b85fd1f8b95a8fb44
felicianpopa <felician.popa@bootintech.com>
2025-05-14 12:35:18 +0300

    Fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
-export default function Home() {
+import { prisma } from "./utils/db";
+
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      id: true,
+      createdAt: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item, index) => (
+          <h1 key={index}>{item.title}</h1>
+        ))}
+      </div>
     </div>
   );
 }
--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -157,11 +157,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_r5jOfYdtP4iz@ep-yellow-mountain-a2ewgsc4-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
