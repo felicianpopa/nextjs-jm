2e804e992f0f9ec3da8a56d8a80ca72cfb00e999
felicianpopa <felician.popa@bootintech.com>
2025-05-27 11:21:18 +0300

    Server actions

---
+++ src/app/dashboard/create/page.tsx
@@ -0,0 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { handleSubmission } from "@/app/actions";
+
+export default function CreateBlogRoute() {
+  return (
+    <div className="flex justify-center align-middle">
+      <Card className="">
+        <CardHeader>
+          <CardTitle>Create Post</CardTitle>
+          <CardDescription>Create a new post</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={handleSubmission} className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Title</Label>
+              <Input
+                required
+                type="text"
+                id="title"
+                placeholder="Title"
+                name="title"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="content">Content</Label>
+              <Textarea required id="content" name="content" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="image">Image URL</Label>
+              <Input required type="text" id="image" name="image" />
+            </div>
+            <Button className="cursor-pointer">Create post</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,58 @@
-export default function Dashboard() {
-  return <h1 className="text-3xl font-bold tracking-tight mb-8">Dashboard</h1>;
+import { prisma } from "@/utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+export default async function Dashboard() {
+  const { getUser } = getKindeServerSession();
+  const userData = await getUser();
+  async function getData() {
+    const data = await prisma.blogPost.findMany({
+      where: {
+        authorId: userData?.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return data;
+  }
+
+  const blogData = await getData();
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Blog articles</h2>
+        <Link
+          className="border-2 p-2 rounded-2xl border-amber-400 bg-amber-100 hover:bg-amber-300 transition-all"
+          href="/dashboard/create"
+        >
+          Create post
+        </Link>
+      </div>
+      <div className="grid gap-5 grid-cols-3">
+        {blogData.map((article) => (
+          <Link key={article.id} href={`/post/${article.id}`}>
+            <Card className="">
+              <CardHeader>
+                <CardTitle>
+                  <h2 className="font-medium text-xl">{article.title}</h2>
+                </CardTitle>
+                <CardDescription>
+                  <p>{article.content}</p>
+                </CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
